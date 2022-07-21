6bd7c892932efffeda3117f3c3477b6bbab105cc
Putta Khunchalee <ultimaweapon@outlook.com>
2022-07-21 09:42:52 +0700

    Updates MDL

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use self::session::Session;
 use locenv::api::LuaState;
-use locenv::FunctionEntry;
+use locenv::{create_table, new_userdata, push_value, set_functions, upvalue_index, FunctionEntry};
 use locenv_macros::loader;
 use std::os::raw::c_int;
 
@@ -14,13 +14,14 @@ const MODULE_FUNCTIONS: [FunctionEntry; 1] = [FunctionEntry {
 const TYPE_SESSION: &str = "session";
 
 extern "C" fn newsession(lua: *mut LuaState) -> c_int {
-    locenv::new_userdata(lua, Session::new());
+    new_userdata(lua, upvalue_index(1), Session::new());
     1
 }
 
 #[loader]
 extern "C" fn loader(lua: *mut LuaState) -> c_int {
-    locenv::create_table(lua, 0, MODULE_FUNCTIONS.len() as _);
-    locenv::set_functions(lua, &MODULE_FUNCTIONS, 0);
+    create_table(lua, 0, MODULE_FUNCTIONS.len() as _);
+    push_value(lua, 2); // Push Context.
+    set_functions(lua, &MODULE_FUNCTIONS, 1);
     1
 }
--- src/session.rs
+++ src/session.rs
@@ -1,6 +1,6 @@
 use crate::TYPE_SESSION;
 use locenv::api::LuaState;
-use locenv::{MethodEntry, Object, UserData};
+use locenv::{check_string, error, upvalue_index, Context, MethodEntry, Object, UserData};
 use std::os::raw::c_int;
 use std::process::{Command, Stdio};
 
@@ -12,26 +12,27 @@ impl Session {
     }
 
     fn run(&mut self, lua: *mut LuaState) -> c_int {
-        let command = locenv::check_string(lua, 2);
+        let context = Context::from_lua(lua, upvalue_index(1));
+        let command = check_string(lua, 2);
 
         // Launch sh.
         let mut launcher = Command::new("sh");
 
-        launcher.current_dir(locenv::get_working_directory());
+        launcher.current_dir(context.working_directory());
         launcher.arg("-ec");
         launcher.arg(&command);
         launcher.stdin(Stdio::null());
 
         let status = match launcher.status() {
             Ok(r) => r,
-            Err(e) => locenv::error!(lua, "failed to launch sh: {}", e),
+            Err(e) => error!(lua, "failed to launch sh: {}", e),
         };
 
         // Check status.
         if !status.success() {
             match status.code() {
-                Some(v) => locenv::error!(lua, "process exited with status {}", v),
-                None => locenv::error!(lua, "process terminated by signal"),
+                Some(v) => error!(lua, "process exited with status {}", v),
+                None => error!(lua, "process terminated by signal"),
             }
         }
 
